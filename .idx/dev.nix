f9f014d4938210f533458c460787526f199c06cd
vinicius.abner1039 <vinicius.abner1039@gmail.com>
2026-03-11 21:56:50 +0000

    attempts to add potcss

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+{pkgs}: {
+  channel = "unstable";
+  packages = [
+    pkgs.nodejs_24
+    pkgs.corepack
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
